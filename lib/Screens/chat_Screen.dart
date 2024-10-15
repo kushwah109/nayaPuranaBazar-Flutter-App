f42f1ddf7ecb605864a726988caec88b567c4b14
kushwah109 <kushwahnirbhika03@gmail.com>
2024-10-15 18:57:38 +0530

    initial commit
    this in commercial Flutter application UI

--- lib/Screens/chat_Screen.dart
+++ lib/Screens/chat_Screen.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
-import 'package:csv/csv.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
+
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
